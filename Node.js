b29e4ed9404edec63ca3a871bf5f7a234b1d0629
mzwd6k9hqh-art <mzwd6k9hqh@privaterelay.appleid.com>
2025-12-20 21:35:28 +0100

    Add API endpoints for members and products

--- Node.js
+++ Node.js
@@ -17,3 +17,27 @@ app.get("/me", async (req, res) => {
 });
 
 app.listen(3000);
+import express from "express";
+import fetch from "node-fetch";
+
+const app = express();
+
+app.get("/members", async (req, res) => {
+  const r = await fetch("https://api.whop.com/api/v2/members", {
+    headers: {
+      Authorization: `Bearer YOUR_WHOP_API_KEY`
+    }
+  });
+  res.json(await r.json());
+});
+
+app.get("/products", async (req, res) => {
+  const r = await fetch("https://api.whop.com/api/v2/products", {
+    headers: {
+      Authorization: `Bearer YOUR_WHOP_API_KEY`
+    }
+  });
+  res.json(await r.json());
+});
+
+app.listen(3000);
